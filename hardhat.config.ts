a8f55bf9c443f37222647c488da787c5df685860
放牛极客 <l_iupeiyu@qq.com>
2022-11-18 14:07:32 +0800

    优化部署配置

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -18,10 +18,7 @@ const config: HardhatUserConfig = {
   networks: {
     localhost:{
       url: "http://127.0.0.1:7545",
-      accounts: [
-        '0xe8d69ddac1fd89af712ee2df89cdb431985e01182fafcf8964e3ae229638827f', 
-        '0x870ae39708a61126178f0bbc9c1ac3c36738285d7df0e657c3315d4e5ca2d19c',
-        '0xf6c38467cfd021cf72146afb6b664e902d22bf11678d852d6ab2c677e615eb22']
+      accounts: [`0x${PRIVATE_KEY}`]
     },
     goerli: {
       url: API,
